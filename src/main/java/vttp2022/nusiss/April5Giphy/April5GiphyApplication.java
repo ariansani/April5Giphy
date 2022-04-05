b9d6781504a2b89a28267bab1f55a2c725f4a7e3
Arian Sani <ariansani@hotmail.com>
2022-04-05 11:04:59 +0800

    initial commit

---
+++ src/main/java/vttp2022/nusiss/April5Giphy/April5GiphyApplication.java
@@ -0,0 +1,13 @@
+package vttp2022.nusiss.April5Giphy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class April5GiphyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(April5GiphyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vttp2022/nusiss/April5Giphy/April5GiphyApplicationTests.java
@@ -0,0 +1,13 @@
+package vttp2022.nusiss.April5Giphy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class April5GiphyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
