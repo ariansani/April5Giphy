228b752fc2e1c013340681ddf4361eace9b45600
Arian Sani <ariansani@hotmail.com>
2022-04-05 12:20:16 +0800

    added giphy

---
+++ src/main/java/vttp2022/nusiss/April5Giphy/controllers/GiphyController.java
@@ -0,0 +1,43 @@
+package vttp2022.nusiss.April5Giphy.controllers;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import vttp2022.nusiss.April5Giphy.models.Giphy;
+import vttp2022.nusiss.April5Giphy.services.GiphyService;
+
+@Controller
+@RequestMapping("/search")
+public class GiphyController {
+
+    @Autowired
+    private GiphyService giphySvc;
+    
+    @GetMapping
+    public String search(
+        @RequestParam(name="q") String q,
+        @RequestParam(name="limit", defaultValue = "10") Integer limit,
+        @RequestParam(name="offset", defaultValue = "0") Integer offset,
+        @RequestParam(name="rating") String rating,
+        @RequestParam(name="lang") String lang,
+        Model model
+    ){
+        Optional <List<Giphy>> giphy = giphySvc.findGiphy(q, limit, offset, rating, lang);
+
+        if(giphy.isEmpty()){
+            return "404";
+        }
+        List<Giphy> validGiphy = giphy.get();
+        System.out.println(">>>>>>>>>"+validGiphy.get(1).getImageUrl().toString());
+        model.addAttribute("giphyList",validGiphy);
+        return "result";
+    }
+
+}
