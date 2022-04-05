ea6a4b691050802d6a207fccbec9986ecc03c3e3
Arian Sani <ariansani@hotmail.com>
2022-04-05 14:59:49 +0800

    added test coverage

--- src/test/java/vttp2022/nusiss/April5Giphy/April5GiphyApplicationTests.java
+++ src/test/java/vttp2022/nusiss/April5Giphy/April5GiphyApplicationTests.java
@@ -1,15 +1,31 @@
 package vttp2022.nusiss.April5Giphy;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import vttp2022.nusiss.April5Giphy.models.Giphy;
+import vttp2022.nusiss.April5Giphy.services.GiphyService;
 
 @SpringBootTest
 class April5GiphyApplicationTests {
 
+	@Autowired
+	private GiphyService giphySvc;
+
 	@Test
-	void contextLoads() {
+	void shouldLoad10Images() {
+		Optional<List<Giphy>> gifs = giphySvc.findGiphy("dog", 10, 0, "g", "en", "original");
+		assertTrue(gifs.isPresent());
 	}
 
-	
+
+
+
 
 }
